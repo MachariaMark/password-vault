870b4b3795c6b0e40675e16b7a84fd8694612bbb
1offmark <official1offmark@gmail.com>
2021-09-04 15:43:40 +0300

    added random, pyperclip, time, termcolor, getpass, credential & user modules to run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import random
+import pyperclip
+import time
+from termcolor import colored, cprint
+from getpass import getpass
+from credential import Credential
+from user import User
