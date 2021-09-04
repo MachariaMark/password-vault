5c66642a0d65fd8410129e62058cd8ba6bfaa07d
1offmark <official1offmark@gmail.com>
2021-09-04 16:18:59 +0300

    declared variables & added save_credential and delete_credential methods to credentials.py

--- credentials.py
+++ credentials.py
@@ -14,3 +14,22 @@ class Credential:
   """
   #class variable; accessible within the entire class
   credential_list = []
+
+def __init__(self,platform,username,email,password):
+  #self represents the instance of the variable
+  self.platform = platform
+  self.username = username
+  self.email = email
+  self.password = password
+    
+def save_credential(self):
+  """
+  this method appends new object to credential_list
+  """
+  Credential.credential_list.append(self)
+
+def delete_credential(self):
+  '''
+  this method deletes a saved credential from the credential_list
+  '''
+  Credential.credential_list.remove(self)
