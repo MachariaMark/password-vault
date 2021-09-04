b9ca4cd7bbc1068ffa81c43969a62938de7496f9
1offmark <official1offmark@gmail.com>
2021-09-04 15:52:28 +0300

    added create_credential, save_credential, delete_credential, find_credentials functions to run.py

--- run.py
+++ run.py
@@ -8,17 +8,44 @@ from credential import Credential
 from user import User
 
 def create_user(login_name, pin):
-    """
-    Function to create a new user
-    """
-    new_user = User(login_name,pin)
-    return new_user
+  """
+  Function to create a new user
+  """
+  new_user = User(login_name,pin)
+  return new_user
 
 def save_user(user):
-    """
-    Function to save user details
-    """
-    user.save_user()
+  """
+  Function to save user details
+  """
+  user.save_user()
 
 def authenticate_user(username,password):
-    return User.user_auth(username,password)
+  return User.user_auth(username,password)
+
+def create_credential(platform,username,email,password):
+  """
+  Function to create a new credential
+  """
+  new_credential = Credential(platform,username,email,password)
+  return new_credential
+
+def save_credential(credential):
+  """
+  Function to save credential
+  """
+  credential.save_credential()
+
+
+def delete_credential(credential):
+  """
+  Function to delete a credential
+  """
+  credential.delete_credential()
+
+
+def find_credentials(platform):
+  """
+  Function that finds a credential by platform name and returns the credentials
+  """
+  return Credential.find_by_platform(platform)
