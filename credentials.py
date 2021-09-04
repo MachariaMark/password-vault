8eaa5160e40c78c14fd116619a8b319e722563ee
1offmark <official1offmark@gmail.com>
2021-09-04 16:14:11 +0300

    added credential method to credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,16 @@
 import pyperclip
 import string
 import random
+
+class Credential:
+  """
+  a contact class
+  __init__ method that helps us define properties for our objects.
+  Args:
+  platform: New credential platform name.(str)
+  username : New username.(str)
+  email: New credential email adress.(str)
+  password : New credential password.(str)
+  """
+  #class variable; accessible within the entire class
+  credential_list = []
