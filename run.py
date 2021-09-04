90f69604050d096b40b517c9c9ac6f5b33555285
1offmark <official1offmark@gmail.com>
2021-09-04 15:46:49 +0300

    added create_user, save_user & authenticate_user functions to run.py

--- run.py
+++ run.py
@@ -6,3 +6,19 @@ from termcolor import colored, cprint
 from getpass import getpass
 from credential import Credential
 from user import User
+
+def create_user(login_name, pin):
+    """
+    Function to create a new user
+    """
+    new_user = User(login_name,pin)
+    return new_user
+
+def save_user(user):
+    """
+    Function to save user details
+    """
+    user.save_user()
+
+def authenticate_user(username,password):
+    return User.user_auth(username,password)
